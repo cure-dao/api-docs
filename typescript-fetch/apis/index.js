197a45a6db5d7991f8cffe0b1210708b4433aed0
Mike P. Sinn <m@quantimodo.com>
2022-06-23 21:59:45 -0500

    Typescript Fetch SDK
    Took 1 hour 15 minutes

---
+++ typescript-fetch/apis/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./AnalyticsApi"), exports);
+__exportStar(require("./AppSettingsApi"), exports);
+__exportStar(require("./AuthenticationApi"), exports);
+__exportStar(require("./ConnectorsApi"), exports);
+__exportStar(require("./FeedApi"), exports);
+__exportStar(require("./MeasurementsApi"), exports);
+__exportStar(require("./NotificationsApi"), exports);
+__exportStar(require("./RemindersApi"), exports);
+__exportStar(require("./SharesApi"), exports);
+__exportStar(require("./StudiesApi"), exports);
+__exportStar(require("./UnitsApi"), exports);
+__exportStar(require("./UserApi"), exports);
+__exportStar(require("./VariablesApi"), exports);
+//# sourceMappingURL=index.js.map
